45c1df883260f770f857c6826c8678bd2431df53
sergeGalkin <sunlit.sergey@gmail.com>
2014-03-02 14:27:32 +0200

    Spring security custom form and error message

---
+++ src/main/java/com/controller/LoginController.java
@@ -0,0 +1,52 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: serge
+ * Date: 3/2/14
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value="/welcome", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal ) {
+
+        String name = principal.getName();
+        model.addAttribute("username", name);
+        model.addAttribute("message", "Spring Security Custom Form example");
+        return "hello";
+
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+
+        return "login";
+
+    }
+
+    @RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+    public String loginerror(ModelMap model) {
+
+        model.addAttribute("error", "true");
+        return "login";
+
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logout(ModelMap model) {
+
+        return "login";
+
+    }
+}
